19f3a7b8bf7ac8845d6808c4b53784bc05b1a587
Feeeeeeeeeela <filipplachycz@gmail.com>
2020-11-25 17:14:59 +0100

    main 25.11.

--- webface/routes.py
+++ webface/routes.py
@@ -1,92 +1,119 @@
 from . import app
-from flask import render_template, request, redirect, url_for, session, flash
-import functools
-from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User, Shortener
+from flask import render_template, request, session, redirect, url_for, g
+from os import urandom
 from pony.orm import db_session
-import string
-import random
-
-# from werkzeug.security import check_password_hash
+from . models import User
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
-def login_required(function):
-    @functools.wraps(function)
-    def wrapper(*args, **kwargs):
-        if "user" in session:
-            return function(*args, **kwargs)
-        else:
-            return redirect(url_for("login", url=request.path))
-
-    return wrapper
-
-
-@app.route("/", methods=["GET"])
+@app.route('/')
 def index():
-    shortcut = "".join([random.choice(string.ascii_letters) for i in range(7)])
-    print(shortcut)
+    title = 'Index'
+    return render_template('base.html.j2', title=title, user=session.get("user"))
 
-    if "user" in sessin:
-        user = User.get(login=session["user"])
-        for addr in user.addresses:
-            print(addr.shortcut, addr.url)
+@app.route('/info/')
+def info():
+    title = 'Info'
+    return render_template('info.html.j2', title=title)
 
-    return render_template("base.html.j2")
+@app.route('/Květák')
+def kvetak():
+    if g.user:
+        title = 'Květák'
+        return render_template('kvetak.html.j2', title=title)
+    return redirect(url_for('login'))
+
+@app.route('/Kapusta')
+def kapusta():
+    if g.user:
+        title = 'Kapusta'
+        return render_template('kapusta.html.j2', title=title)
+    return redirect(url_for('login'))
+
+@app.route('/Banány')
+def banany():
+    if g.user:
+        title = 'Banány'
+        return render_template('banany.html.j2', title=title)
+    return redirect(url_for('login'))
+
+@app.route('/Kalkulačka', methods=["GET", "POST"])
+def formulky():
+    if g.user:
+        label = request.form.get("text")
+        if label:
+            try:
+                result = eval(label)
+            except:
+                result = "Error :)"
+        else:
+            result = ""
+        title = 'Kalkulačka'
+        return render_template('formula.html.j2', title=title, result=result)
+    return redirect(url_for('login'))
 
 
-@app.route("/<string:shortcut>", methods=["GET"])
-def short_redirect(shortcut):
-    shortener = Shortener.get(shortcut=shortcut)
-    if shortener.user:
-        print(shortener.user.login)
-    return render_template("base.html.j2")
+app.secret_key = urandom(24)
 
-
-@app.route("/adduser/", methods=["GET"])
-def adduser():
-    return render_template("adduser.html.j2")
-
-
-@app.route("/adduser/", methods=["POST"])
+@app.route('/login', methods=["GET", "POST"])
 @db_session
-def adduser_post():
-    login = None
-    passwd1 = None
-    passwd2 = None
-    user = User.get(email="nozka@spseol.cz")
-    user = User[login]  # do [] jen primární klíč
-    if user:
-        flash("Uživatel již existuje. Zvolte si jiné uživatelské jméno.")
-        print(user.login, user.password)
-    if len(passwd1) >= 5 and passwd1 == passwd2:
-        user = User(login=login, password=generate_password_hash(passwd1))
-        flash("účet vytvořen")
-    else:
-        flash("hesla nejsou stejná, nebo jsou příliš krátká")
-        return redirect(url_for("adduser"))
-    return redirect(url_for("index"))
-
-
-@app.route("/login/")
 def login():
-    return render_template("login.html.j2")
+    title = 'login'
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.get(login=username)
+        if user:
+            if check_password_hash(user.password, password):
+                session["user"] = user.login
+                return redirect(url_for("index"))
+            else:
+                return render_template('login.html.j2', title=title, error="Špatný heslo kolego")
+
+        else:
+            return render_template('login.html.j2', title=title, error="Špatný jméno kolego")
 
 
-@app.route("/login/", methods=["POST"])
-def login_post():
-    login = None
-    passwd = None
-    user = User[login]
-    if user and check_password_hash(user.password, passwd):
-        session["user"] = login
-        flash("Uspěšně jsi se přihlásil!")
-    else:
-        flash("Nezprávné přihlašovací údaje!")
+    return render_template('login.html.j2', title=title)
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+
+@app.route('/')
+def dropsession():
+    session.pop('user', None)
+    return render_template('login.html.j2')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html.j2')
+
+@app.route("/register", methods=["GET","POST"])
+@db_session
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+        existing = User.get(login=username)
+        if username and not existing and password == password2:
+            user = User(login=username,password=generate_password_hash(password))
+            session["user"] = user.login
+            return redirect(url_for("index"))
+        else:
+            return render_template("register.html.j2", error="Máš tam chybu")
+
+    return render_template("register.html.j2")
+
+@app.route("/logout")
+def logout():
+    g.user = None
+    session["user"] = ""
     return redirect(url_for("index"))
 
-
-@app.route("/logout/")
-def logout():
-    session.pop("user", None)
-    flash("Odhlášeno")
-    return render_template("base.html.j2")
+@app.route("/short", methods=["GET","POST"])
+def short():
+    pass
